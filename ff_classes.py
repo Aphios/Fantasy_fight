0828c5231350e6fe4bdac30daf11ac0c595b05b1
Aphios <sphblanchard@gmail.com>
2020-03-19 10:45:56 +0100

    Character init and repr implemented and tested Autogen settings function implemented and tested

--- ff_classes.py
+++ ff_classes.py
@@ -12,6 +12,23 @@ This file contains the classes :
 
 """
 
+# Constants used to initialize characters
+
+MALE_NAMES = ['Albert', 'Kronos', 'Alec', 'Bran', 'Urtuk', 'Shun']
+FEMALE_NAMES = ['Lydia', 'Tulie', 'Aimee', 'Selene', 'Kira', 'Ksyncix']
+OTHER_NAMES = ['Al', 'Effen', 'Juno', 'Lerrewen', 'Gorgo', 'Jviz']
+GENDERS = ['Male', 'Female', 'Other']
+RACES = ['Githzerai', 'Rakshasa', 'Illithid', 'Tieflin', 'Banshee']
+ABILITIES = {'Githzerai': {'name': 'Vicious Swash', 'damage_min': -10, 'damage_max': 10},
+             'Rakshasa': {'name': 'Subjugate', 'damage_min': -10, 'damage_max': 10},
+             'Illithid': {'name': 'Mind pump', 'damage_min': -10, 'damage_max': 10},
+             'Tieflin': {'name': 'Sting whip', 'damage_min': -10, 'damage_max': 10},
+             'Banshee': {'name': 'Scream', 'damage_min': -10, 'damage_max': 10}}
+PROTECTION_PTS = {'Githzerai': 5, 'Rakshasa': 10, 'Illithid': 3, 'Tieflin': 12, 'Banshee': 8}
+LIFE_PTS = {'Githzerai': 30, 'Rakshasa': 38, 'Illithid': 25, 'Tieflin': 35, 'Banshee': 28}
+INTELLIGENCE_PTS = {'Githzerai': 15, 'Rakshasa': 10, 'Illithid': 25, 'Tieflin': 8, 'Banshee': 20}
+STRENGTH_PTS = {'Githzerai': 12, 'Rakshasa': 20, 'Illithid': 10, 'Tieflin': 18, 'Banshee': 8}
+
 class Character:
     """
     Character is the base class for the player character and their enemies.
@@ -20,51 +37,46 @@ class Character:
     They can equip an armour, a spell and a weapon.
     """
 
-    ABILITIES: {'Githzerai': {'name': 'Vicious Swash', 'damage_min': -10, 'damage_max': 10},
-                'Rakshasa': {'name': 'Subjugate', 'damage_min': -10, 'damage_max': 10},
-                'Illithid': {'name': 'Mind pump', 'damage_min': -10, 'damage_max': 10},
-                'Tieflin': {'name': 'Sting whip', 'damage_min': -10, 'damage_max': 10},
-                'Banshee': {'name': 'Scream', 'damage_min': -10, 'damage_max': 10}}
-    PROTECTION_PTS: {'Githzerai': 5, 'Rakshasa': 10, 'Illithid': 3, 'Tieflin': 12, 'Banshee': 8}
-    LIFE_PTS: {'Githzerai': 30, 'Rakshasa': 38, 'Illithid': 25, 'Tieflin': 35, 'Banshee': 28}
-    INTELLIGENCE_PTS: {'Githzerai': 15, 'Rakshasa': 10, 'Illithid': 25, 'Tieflin': 8, 'Banshee': 20}
-    STRENGTH_PTS: {'Githzerai': 12, 'Rakshasa': 20, 'Illithid': 10, 'Tieflin': 18, 'Banshee': 8}
-
     def __init__(self, name, gender, race, level=1, armour='underwear', weapon='fists', spell='None'):
         self.level = level
-        self.name = name
-        self.gender = gender
-        self.race = race
+        self._name = name
+        self._gender = gender
+        self._race = race
         self.armour = armour
         self.weapon = weapon
         self.spell = spell
-        if self.race == 'Githzerai':
-            self.ability = ABILITIES['Githzerai']
+        if self._race == 'Githzerai':
+            self._ability = ABILITIES['Githzerai']
             self.protection = PROTECTION_PTS['Githzerai']
             self.strength = STRENGTH_PTS['Githzerai']
             self.life = LIFE_PTS['Githzerai']
             self.intelligence = INTELLIGENCE_PTS['Githzerai']
-        elif self.race == 'Rakshasa':
-            self.ability = ABILITIES['Rakshasa']
+        elif self._race == 'Rakshasa':
+            self._ability = ABILITIES['Rakshasa']
             self.protection = PROTECTION_PTS['Rakshasa']
             self.strength = STRENGTH_PTS['Rakshasa']
             self.life = LIFE_PTS['Rakshasa']
             self.intelligence = INTELLIGENCE_PTS['Rakshasa']
-        elif self.race == 'Illithid':
-            self.ability = ABILITIES['Illithid']
+        elif self._race == 'Illithid':
+            self._ability = ABILITIES['Illithid']
             self.protection = PROTECTION_PTS['Illithid']
             self.strength = STRENGTH_PTS['Illithid']
             self.life = LIFE_PTS['Illithid']
             self.intelligence = INTELLIGENCE_PTS['Illithid']
-        elif self.race == 'Tieflin':
-            self.ability = ABILITIES['Tieflin']
+        elif self._race == 'Tieflin':
+            self._ability = ABILITIES['Tieflin']
             self.protection = PROTECTION_PTS['Tieflin']
             self.strength = STRENGTH_PTS['Tieflin']
             self.life = LIFE_PTS['Tieflin']
             self.intelligence = INTELLIGENCE_PTS['Tieflin']
-        elif self.race == 'Banshee':
-            self.ability = ABILITIES['Banshee']
+        elif self._race == 'Banshee':
+            self._ability = ABILITIES['Banshee']
             self.protection = PROTECTION_PTS['Banshee']
             self.strength = STRENGTH_PTS['Banshee']
             self.life = LIFE_PTS['Banshee']
             self.intelligence = INTELLIGENCE_PTS['Banshee']
+
+    def __repr__(self):
+        return f"{self._name}, {self._gender}, {self._race}, ability : {self._ability['name']}, level : {self.level}" \
+               f", armour : {self.armour}, life : {self.life}, strength : {self.strength}, intelligence : " \
+               f"{self.intelligence}, protection : {self.protection}"
--- ff_func.py
+++ ff_func.py
@@ -10,18 +10,18 @@ This file contains the functions used in the main program.
 """
 
 import random
-import ff_constants as cst
+import ff_classes as ffc
 
 def autogen():
     """Generates random settings for a character.
     The function returns a name, a gender and a race randomly selected from set constants in ff_constants.
     """
-    gender = random.choice(cst.GENDERS)
-    race = random.choice(cst.RACES)
+    gender = random.choice(ffc.GENDERS)
+    race = random.choice(ffc.RACES)
     if gender == 'Male':
-        name = random.choice(cst.MALE_NAMES)
+        name = random.choice(ffc.MALE_NAMES)
     elif gender == 'Female':
-        name = random.choice(cst.FEMALE_NAMES)
+        name = random.choice(ffc.FEMALE_NAMES)
     else:
-        name = random.choice(cst.OTHER_NAMES)
+        name = random.choice(ffc.OTHER_NAMES)
     return {'name' : name, 'gender' : gender, 'race' : race}
