3f29390d037e87d7f439cdcf5e17f233bac1cc6c
Aphios <sphblanchard@gmail.com>
2020-03-21 16:00:32 +0100

    [EVO] Git ignore

---
+++ ff_main.py
@@ -0,0 +1,17 @@
+"""Fantasy Fight Project
+
+-- Version 0.1
+Author : Sophie Blanchard
+Purpose : simple fight game with fantasy characters
+Start date : 03-17-2020
+Last update : 03-17-2020
+
+This file contains the main program.
+"""
+
+if __name__ == '__main__':
+    import random
+    import ff_classes as ffc
+    import ff_func as fff
+
+# Character creation
