2fb6fc1875c794062e718d96e92aafddf30a9561
Aphios <sphblanchard@gmail.com>
2020-04-16 15:09:35 +0200

    deleting temp files

---
+++ ff_func.py
@@ -0,0 +1,15 @@
+"""Fantasy Fight Project
+
+-- Version 0.1
+Author : Sophie Blanchard
+Purpose : simple fight game with fantasy characters
+Start date : 03-17-2020
+Last update : 04-14-2020
+
+This file contains some specific short functions used in the main program.
+These functions must not need to import any other ff module.
+"""
+
+
+
+
